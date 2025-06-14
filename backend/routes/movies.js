dd6817a3110adf0ae9f34f5e4be9ae0c522f8fd5
Kelechi A. Nwankwo <kelechiann.nwankwo@gmail.com>
2025-06-15 00:39:07 +0100

    Add watchlist and favorites pages with removal

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -7,12 +7,16 @@ const {
   addFavorite,
   getWatchlist,
   addWatchlist,
+  removeFavorite,
+  removeWatchlist,
 } = require('../controllers/movieController');
 
 router.get('/search', search);
 router.get('/favorites', auth, getFavorites);
 router.post('/favorites', auth, addFavorite);
+router.delete('/favorites/:movieId', auth, removeFavorite);
 router.get('/watchlist', auth, getWatchlist);
 router.post('/watchlist', auth, addWatchlist);
+router.delete('/watchlist/:movieId', auth, removeWatchlist);
 
 module.exports = router;
