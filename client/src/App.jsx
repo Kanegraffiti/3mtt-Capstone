0b770fcafe638f41c7d957a00b790df5951d8343
Kelechi A. Nwankwo <kelechiann.nwankwo@gmail.com>
2025-06-22 21:17:22 +0100

    chore: remove unused logo asset

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
 import Header from './components/Header.jsx';
 import OfflineBanner from './components/OfflineBanner.jsx';
 import Home from './pages/Home.jsx';
-import Login from './pages/Login.jsx';
-import Register from './pages/Register.jsx';
+import AuthForm from './components/AuthForm.jsx';
 import Profile from './pages/Profile.jsx';
 import Library from './pages/Library.jsx';
 import MovieDetails from './pages/MovieDetails.jsx';
@@ -20,8 +19,8 @@ function App() {
       <div className="pt-16 pb-20">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<AuthForm mode="login" />} />
+        <Route path="/register" element={<AuthForm mode="register" />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/library" element={<Library />} />
