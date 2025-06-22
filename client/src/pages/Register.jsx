ff45fca323bb961be45a0c01d6e0412be3de77b1
Kelechi A. Nwankwo <kelechiann.nwankwo@gmail.com>
2025-06-22 23:06:18 +0100

    Fix search input and button styles

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -25,7 +25,12 @@ const Register = () => {
       <input className="w-full p-2 text-black" value={name} onChange={e => setName(e.target.value)} placeholder="Name" />
       <input className="w-full p-2 text-black" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
       <input type="password" className="w-full p-2 text-black" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
-      <button className="w-full bg-blue-500 py-2" type="submit">Register</button>
+      <button
+        className="w-full bg-purple-600 text-white px-4 py-1 rounded"
+        type="submit"
+      >
+        Register
+      </button>
     </form>
   );
 };
